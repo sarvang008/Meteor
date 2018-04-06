ac93e9a435bbfd6cf252968adad9ae760bb6fc4c
sarvang008 <sarvang.m@somaiya.edu>
2018-04-06 21:24:27 +0530

    commit on 4 april

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -16,20 +16,64 @@ Template.todo.events({
 
 });
 
+Template.update.events({
+
+    'click.btnupdate':function(event) {
+
+    // Set the checked property to the opposite of its current value
+
+     /* var object=$('.add_text').val();
+
+      console.log(object);
+      var data =books.find(name:object);
+
+        console.log(data);
+
+
+    books.update(_id:data._id, {
+      $set: { name:'27' }
+    });
+*/
+      var name=$('.add_text').val();
+      var age1=$('.update_age').val();
+      var doc= books.findOne({name:name});
+     // console.log(doc._id);
+      if(doc!=""&&age1!=""&&name!=""){
+      books.update({_id:doc._id},{$set:{age:age1}});
+      console.log('updated');
+      }
+      else
+      {
+          alert("name not found or feilds empty");
+      }
+      $('.add_text').val("");
+      $('.update_age').val("");
+      
+      
+return false;
+    }
+
+
+
+
+});
 Template.add.events({
 
-'submit.submit':function(event){
+'click.submit':function(event){
 
     var data=$('.add_text').val();
+    var age1=$('.update_age').val();
     console.log(data);
     
  
 
     //console.log(books.find());
     
-    if(data)
+    if(data!=""&&age1!="")
     books.insert({
-              name:data
+              name:data,
+              age:age1
+
 
     }
     
@@ -37,13 +81,14 @@ Template.add.events({
 );
 else
 {
-    alert("Enter text");
+    alert("Fields are empty");
 }
 
 
 //event.target.text.value="";
 $('.add_text').val("");
 
+$('.update_age').val("");
 
     return false;
 }
